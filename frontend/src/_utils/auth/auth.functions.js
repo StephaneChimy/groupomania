68e7b809fd563075beee7ef82f94aa9e882c4e04
Stéphane Chimy <stephane.akira@gmail.com>
2021-01-16 13:22:46 +0100

    Email Regex changed and Add comment on password regex

--- frontend/src/_utils/auth/auth.functions.js
+++ frontend/src/_utils/auth/auth.functions.js
@@ -9,10 +9,10 @@ const CryptoJS = require("crypto-js");
 
 // Variables
 const REGEX = {
-  // eslint-disable-next-line no-useless-escape
-  EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   NAME_REGEX: "^([p{L}]+)([p{L}- ']*)$",
   SURNAME_REGEX: "^([p{L}]+)([p{L}- ']*)$",
+  // Here minimum 4 characters, at least one letter and one number
+  // This needs to be changed in production with a minimum of 8 characters and a maximum.
   PASSWORD_REGEX: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,}$",
 };
 
